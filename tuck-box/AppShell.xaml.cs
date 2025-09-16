cdd522c265cf05aae678fce6d056929b3c7ff55b
Logan Young <younglogan09@gmail.com>
2025-09-17 10:21:40 +1200

    App init + Git ignore

---
+++ tuck-box/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace tuck_box;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ tuck-box/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "tuck_box")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "tuck_box.Pages")]
