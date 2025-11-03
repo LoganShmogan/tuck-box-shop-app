bd141611a77c3d8c95e28a2c96f741148214cf29
Logan Young <younglogan09@gmail.com>
2025-11-03 18:59:41 +1200

    init + packages

---
+++ TuckBoxApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TuckBoxApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ TuckBoxApp/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TuckBoxApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TuckBoxApp.Pages")]
